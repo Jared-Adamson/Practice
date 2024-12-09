9b8c3c0dbdcdb57c281016e638e750e75ae2314d
Jared Adamson <jared.d.adamson@gmail.com>
2024-12-09 11:55:12 -0700

    Create Itinerary.js

---
+++ Itinerary.js
@@ -0,0 +1,20 @@
+function start(){
+	// Write your code here
+	
+	var flights = ["San Francisco", "New York", "Chicago", "Honolulu"];
+	
+	for(var i=0;i<flights.length;i++){
+	    
+	    if(i == flights.length -1){ 
+	        
+	        println(flights[i]);
+	        
+	    }else{
+	        
+	        print(flights[i]+" -> ");
+	        
+	    }
+	    
+	}
+	
+}
