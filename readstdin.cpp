ba78e94bede2aabf3406c3b72c584cd421ee17f3
Jared Adamson <jared.d.adamson@gmail.com>
2024-12-09 13:04:21 -0700

    Create readstdin.cpp

---
+++ readstdin.cpp
@@ -0,0 +1,27 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+
+
+int main() {
+    /* Task Read  numbers from stdin and print their sum to stdout.
+    Enter your code here. Read input from STDIN. Print output to STDOUT */
+    
+    int x;
+    int y = 0;
+    
+    while( cin >> x){
+        
+        y = y+x;
+        
+    }
+    
+    cout << y;
+    
+    return 0;
+}
